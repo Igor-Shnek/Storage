c4f2806b957cf022fdee0b2455dda1cda7560545
Igor Shnek <timmy-taker@mail.ru>
2018-09-03 13:02:20 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string allText = File.ReadAllText(@"H:\input.txt", Encoding.Default);
+            char[] separators = {' ', '.', ',', ';', ':', '"', '!', '?', '{', '}', '(', ')'};
+            string[] words = allText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            List<string> wordsList = words.ToList();
+            wordsList.Sort();
+            List<string> wordsListCopy = new List<string>();
+            List<int> wordsCounter = new List<int>();
+            foreach (string word in wordsList.Select(x => x.ToLower()))
+            {
+                if (!wordsListCopy.Contains(word))
+                {
+                    wordsListCopy.Add(word);
+                    wordsCounter.Add(1);
+                }
+                else wordsCounter[wordsListCopy.IndexOf(word)]++;
+            }
+            wordsList.Clear();
+            foreach (string word in wordsListCopy)
+            {
+                if (!wordsList.Contains(word[0].ToString()))
+                {
+                    wordsList.Add(word[0].ToString());
+                }
+                wordsList.Add(word + " - " + wordsCounter.ElementAt(wordsListCopy.IndexOf(word)));
+            }
+            File.AppendAllLines(@"H:\output.txt", wordsList);
+            Console.Read(); 
+        }
+    }
+}
