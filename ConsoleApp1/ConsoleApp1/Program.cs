52d535180444414042c0959a634fcd591d8aa58a
Igor Shnek <timmy-taker@mail.ru>
2018-09-06 13:19:43 +0200

    Add files via upload
    The project was remaded in object-oriented style. Class "Text", constructor and methods were created.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,33 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string allText = File.ReadAllText(@"H:\input.txt", Encoding.Default);
-            char[] separators = {' ', '.', ',', ';', ':', '"', '!', '?', '{', '}', '(', ')'};
-            string[] words = allText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            List<string> wordsList = words.ToList();
-            wordsList.Sort();
-            List<string> wordsListCopy = new List<string>();
-            List<int> wordsCounter = new List<int>();
-            foreach (string word in wordsList.Select(x => x.ToLower()))
-            {
-                if (!wordsListCopy.Contains(word))
-                {
-                    wordsListCopy.Add(word);
-                    wordsCounter.Add(1);
-                }
-                else wordsCounter[wordsListCopy.IndexOf(word)]++;
-            }
-            wordsList.Clear();
-            foreach (string word in wordsListCopy)
-            {
-                if (!wordsList.Contains(word[0].ToString()))
-                {
-                    wordsList.Add(word[0].ToString());
-                }
-                wordsList.Add(word + " - " + wordsCounter.ElementAt(wordsListCopy.IndexOf(word)));
-            }
-            File.AppendAllLines(@"H:\output.txt", wordsList);
-            Console.Read(); 
+            Text a = new Text(@"H:\input.txt", @"H:\output.txt");
+            a.writeTheFile(a.transformText(a.readTheFile()));
         }
     }
 }
---
+++ ConsoleApp1/ConsoleApp1/Text.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Text
+    {
+        string path;
+        string outputPath;
+        
+        public Text (string path, string outputPath)
+        {
+            this.path = path;
+            this.outputPath = outputPath;
+        }
+
+        public List<string> readTheFile ()
+        {
+            char[] separators = { ' ', '.', ',', ';', ':', '"', '!', '?', '{', '}', '(', ')' };
+            List<string> words = File.ReadAllText(path, Encoding.Default).ToString().Split(separators, StringSplitOptions.RemoveEmptyEntries).ToList();
+            return words;
+        }
+
+        public List<string> transformText (List<string> words)
+        {
+            words.Sort();
+            List<string> wordsCopy = new List<string>();
+            List<int> wordsCounter = new List<int>();
+            foreach (string word in words.Select(x => x.ToLower()))
+            {
+                if (!wordsCopy.Contains(word))
+                {
+                    wordsCopy.Add(word);
+                    wordsCounter.Add(1);
+                }
+                else wordsCounter[wordsCopy.IndexOf(word)]++;
+                
+            }
+            words.Clear();
+            foreach (string word in wordsCopy)
+            {
+                if (!words.Contains(word[0].ToString()))
+                {
+                    words.Add(word[0].ToString());
+                }
+                words.Add(word + " - " + wordsCounter.ElementAt(wordsCopy.IndexOf(word)));
+            }
+            return words;
+        }
+
+        public void writeTheFile (List<string> words)
+        {
+            File.AppendAllLines(outputPath, words);
+        }
+    }
+}
